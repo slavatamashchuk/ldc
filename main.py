349eb51a95f1823266dd1e4701436cae93961260
Slava Tamashchuk <slava.tamashchuk@gmail.com>
2024-01-17 15:36:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 answer = []
 
-a = 20#float(input("Enter your first number.\n"))
-b = 10#float(input("Enter your second number.\n"))
+a = 10#float(input("Enter your first number.\n"))
+b = 2#float(input("Enter your second number.\n"))
 n = 10#int(input("Enter the number digits in the answer.\n"))
 
 for i in range(n):
-    whole_division_result = a // b
-    remainder_from_division = a % b
-    answer.append(whole_division_result)
-    str_answer = "".join(answer)
-    print(str_answer)
+    a = a // b
+    b = a % b
+    answer.append(str(a))
+
+    print("".join(answer))
